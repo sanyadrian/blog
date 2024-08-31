88329037036466da2ad045601da60e97f8b45fb9
sanyadrian <adrianovoleksandr@gmail.com>
2024-08-31 19:34:28 -0400

    Changed view and working on comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page") 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.AllPostView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page") 
+]
