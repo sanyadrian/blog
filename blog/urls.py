da3dfb46a8bdbeb452811b878847b255d5b6902e
sanyadrian <adrianovoleksandr@gmail.com>
2024-08-31 16:08:01 -0400

    Working on serving uploaded files

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page") 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
