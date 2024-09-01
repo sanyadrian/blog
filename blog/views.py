3bfeb0ffad33958f4cddae526d7993d0132b8ccb
sanyadrian <adrianovoleksandr@gmail.com>
2024-08-31 22:07:24 -0400

    Added comments and changed style

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from datetime import date
 from typing import Any
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from .forms import CommentForm
+from django.views import View
 
 class StartingPageView(ListView):
     template_name = "blog/index.html"
@@ -16,16 +20,36 @@ class StartingPageView(ListView):
         return data
 
 class AllPostView(ListView):
-    template_name = "blog/all-posts.html"
-    model = Post
     ordering = ["-date"]
     context_object_name = "all_posts"
 
-class SinglePostView(DetailView):
+class SinglePostView(View):
     model = Post
     template_name = "blog/post-detail.html"
-    context_object_name = "post"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["tags"] = self.object.tags.all()
-        return context
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+        "post": post,
+        "post_tags": post.tags.all(),
+        "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        
+        context = {
+        "post": post,
+        "post_tags": post.tags.all(),
+        "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
